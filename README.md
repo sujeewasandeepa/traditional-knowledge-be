da48787e8ae8c53e1605ee878418ae13c6f3defe
Sujeewa Sandeepa Kularathna <sujeewasandeepa@proton.me>
2023-06-04 17:02:34 +0530

    implement creating a new info

---
+++ README.md
@@ -0,0 +1,3 @@
+to create the container: sudo docker run --name my-postgres -e POSTGRES_PASSWORD=adminpassword -p 5432:5432 -d postgres
+
+to start the container: sudo docker start my-postgres
--- index.ts
+++ index.ts
@@ -1,29 +1,30 @@
 import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
 import connectDB from './db';
 
 const app: Express = express();
 
 const port = 4000;
 
+// middleware
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
 	res.send("Express + Typescript server!!");
 });
 
-app.post("/api", async(req:Request, res:Response) => {
+// creating a new info
+app.post("/api/info", async(req:Request, res:Response) => {
 	try {
-		const client = await connectDB(); 
-		const createtable = `
-		      CREATE TABLE users (
-			id SERIAL PRIMARY KEY,
-			name VARCHAR(255),
-			email VARCHAR(255)
-		      );
-		    `;
-
-		await client.query(createtable);
-		client.end();
-		res.status(200).send('table created!');
+    const information = req.body;
+    const createInfoQuery = `
+      INSERT INTO info(id, timestamp, title, author, description, tags) 
+      VALUES ('${information.id}', '${information.timestamp}', '${information.title}',
+        '${information.author}', '${information.description}', '${information.tags}'); 
+    `;
+    console.log(createInfoQuery);
+    const client = await connectDB(); 
+    await client.query(createInfoQuery);
+    res.send("Info entered to the system!");
 
 	} catch (err) {
 		console.error(err);
@@ -33,8 +34,8 @@ app.get("/api", async (req:Request, res:Response) => {
 	try {
 		const pool = await connectDB();
 		const checkTableQuery = `
-		SELECT * FROM users;
-	    `;
+		SELECT * FROM users_new;
+	  `;
 		const { rows } = await pool.query(checkTableQuery);
 		console.log(rows);
 		let result = ["all is well", rows];
